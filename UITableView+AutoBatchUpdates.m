15c81fa8397b93cb05dbd7047a532b988fc9487c
KtioszDev <37155945+ktiOSz@users.noreply.github.com>
2019-06-24 20:30:03 +0200

    Create UITableView+AutoBatchUpdates.m

---
+++ UITableView+AutoBatchUpdates.m
@@ -0,0 +1,29 @@
+#import "UITableView+AutoBatchUpdates.h"
+#import "NSArray+Changes.h"
+
+@implementation UITableView (AutoBatchUpdates)
+
+- (void)applyChanges:(NSArray *)changes toSection:(NSUInteger)section
+{
+    NSMutableArray *inserted = [NSMutableArray array];
+    NSMutableArray *deleted = [NSMutableArray array];
+    
+    for (M13ObjectIndexChange *change in changes) {
+        switch (change.changeType) {
+            case M13ObjectIndexChangeTypeMoved:
+                [self moveRowAtIndexPath:[NSIndexPath indexPathForItem:change.previousIndex inSection:section] toIndexPath:[NSIndexPath indexPathForItem:change.currentIndex inSection:section]];
+                break;
+            case M13ObjectIndexChangeTypeInserted:
+                [inserted addObject:[NSIndexPath indexPathForItem:change.currentIndex inSection:section]];
+                break;
+            case M13ObjectIndexChangeTypeDeleted:
+                [deleted addObject:[NSIndexPath indexPathForItem:change.previousIndex inSection:section]];
+                break;
+            default:
+                break;
+        }
+    }
+    
+    [self insertRowsAtIndexPaths:inserted withRowAnimation:UITableViewRowAnimationAutomatic];
+    [self deleteRowsAtIndexPaths:deleted withRowAnimation:UITableViewRowAnimationAutomatic];
+}
